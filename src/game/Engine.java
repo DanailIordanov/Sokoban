2d05f01dd16872f776094085adfa8c9665e568d4
DanailIordanov <danail.iordanov@yahoo.com>
2019-01-17 23:00:55 +0100

    Implemented the game engine, which is to be loaded from the launcher

--- src/game/Engine.java
+++ src/game/Engine.java
@@ -1,9 +1,87 @@
 package game;
 
+import common.Direction;
+import fields.DynamicField;
+import managers.FieldManager;
+import managers.GameManager;
+import managers.ValidationManager;
+import models.Player;
+
+import java.util.Scanner;
+
 public class Engine {
 
+    private DynamicField dynamicField;
+    private FieldManager field;
+    private ValidationManager validation;
+    private GameManager game;
+    private Scanner scanner;
+
+    public Engine(DynamicField dynamicField,
+                  FieldManager field, ValidationManager validation,
+                  GameManager game, Scanner scanner) {
+        this.dynamicField = dynamicField;
+        this.field = field;
+        this.validation = validation;
+        this.game = game;
+        this.scanner = scanner;
+    }
+
     public void run() {
-        //Here should be put all the logic for the game
+        var player = dynamicField.getEntities(Player.class).get(0);
+
+        while (true) {
+            field.hide();
+
+            System.out.print(field.show());
+
+            if(game.isWon()) {
+                break;
+            }
+
+            var direction = this.parseDirection();
+
+            if (!validation.willCollideIn(direction, player)) {
+                var targetLocation = player.getManipulatedLocation(direction);
+
+                if (validation.boxShouldBeMovedIn(direction)) {
+                    var box = dynamicField.getEntity(targetLocation.getRow(), targetLocation.getColumn());
+                    if(!validation.willCollideIn(direction, box)) {
+                        field.moveIn(direction, box);
+                    } else {
+                        continue;
+                    }
+                }
+
+                field.moveIn(direction, player);
+            }
+        }
+
+        System.out.println("Congratulations! You have won the game!");
+    }
+
+    private Direction parseDirection() {
+        String command = "";
+
+        var commandIsValid = false;
+        while (!commandIsValid) {
+            try {
+                command = this.scanner.next();
+                this.validation.checkInput(command);
+                commandIsValid = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        switch (command) {
+            case "w": return Direction.Up;
+            case "s": return Direction.Down;
+            case "d": return Direction.Right;
+            case "a": return Direction.Left;
+        }
+
+        return null;
     }
 
 }
--- src/game/Launcher.java
+++ src/game/Launcher.java
@@ -1,12 +1,28 @@
 package game;
 
+import fields.DynamicField;
+import fields.StaticField;
+import managers.FieldManager;
+import managers.GameManager;
+import managers.ValidationManager;
+
+import java.util.Scanner;
+
 public class Launcher {
 
     public static void main(String[] args) {
+        var staticField = new StaticField();
+        var dynamicField = new DynamicField();
+        var field = new FieldManager(staticField, dynamicField);
+        var validation = new ValidationManager(staticField, dynamicField);
+        var game = new GameManager(staticField, dynamicField);
 
-        var engine = new Engine(/* if parameters are required for the engine they can go here */);
+        var scanner = new Scanner(System.in);
+
+        var engine = new Engine(dynamicField, field, validation, game, scanner);
         engine.run();
 
+        scanner.close();
     }
 
 }
